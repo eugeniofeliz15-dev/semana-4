2553c694e600a1ed5df961a95dab6940ef2e2cdd
CristianClaudio <eugenio.feliz15@gmail.com>
2026-08-24 20:13:25 -0400

    feat: conexión a postgres y GET /api/menu

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
 import express, { type Request, type Response } from "express"; 
 import swaggerRouter from "./routes/swagger.router.js";
-import cors from "cors"
+import cors from "cors";
+import pool from "./config/db.js";
 
 const port = process.env.PORT; 
 
-
-
 const app = express();
 
-// Middlewares 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use("/api/docs", swaggerRouter) 
-
+app.use("/api/docs", swaggerRouter); 
 
 app.get("/", (req: Request, res: Response) => {
-    /*#swagger.tags = ['Tests']*/
+    
     res.json({
         status: "Server online",
         version: "1.0.0"
     });
+});
+
+app.get("/api/menu", async (req: Request, res: Response) => {
+   
+    try {
+        const result = await pool.query("SELECT * FROM productos");
+        res.json(result.rows);
+    } catch (error) {
+        console.error("Error al consultar el menú:", error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
 });
 
 app.listen(port, () => {
